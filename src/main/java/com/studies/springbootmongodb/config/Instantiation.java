918790f64e8df0173f635eb4887cf46b7ea520aa
Gabrielly Freire <gabylopesfreire@gmail.com>
2024-02-04 11:43:57 -0300

    feat: Operação de instanciação da base de dados

---
+++ src/main/java/com/studies/springbootmongodb/config/Instantiation.java
@@ -0,0 +1,26 @@
+package com.studies.springbootmongodb.config;
+
+import com.studies.springbootmongodb.domain.User;
+import com.studies.springbootmongodb.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Arrays;
+
+@Configuration
+public class Instantiation implements CommandLineRunner {
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Override
+    public void run(String... args) throws Exception {
+
+        User maria = new User(null, "Maria Brown", "maria@gmail.com");
+        User alex = new User(null, "Alex Green", "alex@gmail.com");
+        User bob = new User(null, "Bob Grey", "bob@gmail.com");
+
+        userRepository.saveAll(Arrays.asList(maria, alex, bob));
+    }
+}
