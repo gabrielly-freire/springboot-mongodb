fca26611b525959be8bd40b9957326f82803e2a4
Gabrielly Freire <gabylopesfreire@gmail.com>
2024-02-06 14:44:51 -0300

    feat: Acrescentando comentários aos posts

--- src/main/java/com/studies/springbootmongodb/config/Instantiation.java
+++ src/main/java/com/studies/springbootmongodb/config/Instantiation.java
@@ -3,6 +3,7 @@ package com.studies.springbootmongodb.config;
 import com.studies.springbootmongodb.domain.Post;
 import com.studies.springbootmongodb.domain.User;
 import com.studies.springbootmongodb.dto.AuthorDto;
+import com.studies.springbootmongodb.dto.CommentDto;
 import com.studies.springbootmongodb.repositories.PostRepository;
 import com.studies.springbootmongodb.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -39,6 +40,13 @@ public class Instantiation implements CommandLineRunner {
 
         Post post1 = new Post(null, simpleDateFormat.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", new AuthorDto(maria));
         Post post2 = new Post(null, simpleDateFormat.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", new AuthorDto(maria));
+
+        CommentDto comment1 = new CommentDto("Boa viagem mano!", simpleDateFormat.parse("21/03/2018"), new AuthorDto(alex));
+        CommentDto comment2 = new CommentDto("Aproveite!", simpleDateFormat.parse("22/03/2018"), new AuthorDto(bob));
+        CommentDto comment3 = new CommentDto("Tenha um ótimo dia!", simpleDateFormat.parse("23/03/2018"), new AuthorDto(alex));
+
+        post1.getComments().addAll(Arrays.asList(comment1, comment2));
+        post2.getComments().add(comment3);
 
         postRepository.saveAll(Arrays.asList(post1, post2));
 
